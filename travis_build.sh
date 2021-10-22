75149559e350d723ac3f842ccd7341294ac1e16c
Maximilian Keßler <git@maximilian-kessler.de>
2021-10-22 09:57:47 +0200

    travis build script: retrieve old builds from orgin

--- travis_build.sh
+++ travis_build.sh
@@ -1,19 +1,19 @@
 # ! /bin/sh
-TRAVIS_BRANCH=algebraic-geometr
+TRAVIS_BRANCH=algebraic-geometry
 
 rm -rf LatexPackagesBuild
 
 git clone https://github.com/kesslermaximilian/LatexPackagesBuild.git LatexPackagesBuild
 cd LatexPackagesBuild
-FOUND=$(git branch -a | sed -n '/remotes\/origin\/.*-build/p' | sed 's/remotes\/origin\///g' | sed 's/-build//g' | sed 's/[[:space:]]//g' | sed -n "/^${TRAVIS_BRANCH}$/p")
+REMOTE_BRANCH=$(git branch -a | sed -n '/remotes\/origin\/.*-build/p' | sed 's/remotes\/origin\///g' | sed 's/-build//g' | sed 's/[[:space:]]//g' | sed -n "/^${TRAVIS_BRANCH}$/p")
 
-echo ${FOUND}
+echo ${REMOTE_BRANCH}
 
-if [ "$FOUND" = "" ];then
+if [ "$REMOTE_BRANCH" = "" ];then
     git checkout --orphan ${TRAVIS_BRANCH}-build
     ls -ra | sed '/^\.git$/d' | sed '/^\.\.$/d' | sed '/^\.$/d' |  xargs -r git rm --cached
     ls -ra | sed '/^\.git$/d' | sed '/^\.\.$/d' | sed '/^\.$/d' |  xargs -r rm -rf
 else
     echo "found"
-    git checkout -b ${FOUND}-build origin/${FOUND}-build
+    git checkout -b ${REMOTE_BRANCH}-build origin/${REMOTE_BRANCH}-build
 fi
